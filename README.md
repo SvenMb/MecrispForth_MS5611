ec848cc6922241c4b21088d66ec913a9ddab27bd
Sven Muehlberg <SvenMuehlberg@gmail.com>
2020-03-12 15:55:43 +0100

    initial checkin ms5611.fs
    needs a lot of cleanup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MecrispForth_MS5611
 Forth words for ms5611 high resolution pressure sensor on i2c
+
+This was written on an STM32f103, but should run on any STM32 microcontroller.
+
+This is written and tested for Mecrips-Stellaris Forth: http://mecrisp.sourceforge.net/
+
+And I used i2c.fs from Jean-Claude Wippler's flib to simplify the hardware access. https://git.jeelabs.org/jcw/embello/src/branch/master/explore/1608-forth/flib/
+
+Bugs:
+ - development currently not finished
+
+Be aware this is very experimental code!
---
+++ ms5611.fs
@@ -0,0 +1,98 @@
+$77 constant MS5611.ADDR
+
+12 buffer: params \ calibration data
+6 buffer: values \ rawdata
+
+: MS5611-reset 
+  MS5611.ADDR i2c-addr
+  $1e >i2c
+  0 i2c-xfer drop
+;
+
+: ms5611-i2c+ ( addr -- addr+1 )
+ i2c> over c! 1+ 
+;
+
+: ms5611-rd ( addr n reg -- addr+n )
+  MS5611.ADDR i2c-addr >i2c
+  dup i2c-xfer drop
+  0 do ms5611-i2c+ loop
+;
+
+: MS5611-calib
+  
+  params
+  6 0 do
+		2 $a2 I 2 * + ms5611-rd
+  loop
+  drop
+;
+
+: ms5611-rawt
+MS5611.ADDR i2c-addr
+$58 >i2c 0 i2c-xfer drop
+10 ms
+values 3 0 ms5611-rd drop
+values dup c@ 16 lshift swap 1+ dup c@ 8 lshift swap 1+ c@ + +	
+;
+
+: ms5611-rawp
+MS5611.ADDR i2c-addr
+$48 >i2c 0 i2c-xfer drop
+10 ms
+values 3 + 3 0 ms5611-rd drop
+values 3 + dup c@ 16 lshift swap 1+ dup c@ 8 lshift swap 1+ c@ + +	
+;
+
+
+: ms5611-temp
+
+ms5611-rawt
+params dup 8 + c@ 8 lshift swap 9 + c@ + 256 * -
+params dup 10 + c@ 8 lshift swap 11 + c@ + *
+8388608 / 2000 +
+;
+
+: ms5611-press
+	ms5611-rawt
+	params dup 8 + c@ 8 lshift swap 9 + c@ + 256 * -
+	dup
+		
+	params dup 6 + c@ 8 lshift swap 7 + c@ + * 128 / s>d
+	params dup 2 + c@ 8 lshift swap 3 + c@ + 65536 um* d+
+	dnegate
+	
+	rot
+	
+	params dup 4 + c@ 8 lshift swap 5 + c@ + * 256 / s>d
+	params dup 0 + c@ 8 lshift swap 1 + c@ + 32768 um* d+
+	
+	ms5611-rawp s>d ud* 2097152 s>d d/ d+
+	32768 um/mod swap drop
+;
+
+: MS5611-height
+
+;
+
+
+i2c-init
+ms5611-reset
+100 ms
+ms5611-calib
+
+6 buffer: hoehe
+
+: vario. 
+  2200
+  ms5611-press
+  90000 -
+  85 *
+  100 /
+  -
+
+  .
+  
+;
+
+
