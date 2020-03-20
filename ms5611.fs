d80677906a60c98759b722f701a447237a5bb3ab
Sven Muehlberg <SvenMuehlberg@gmail.com>
2020-03-20 10:33:15 +0100

    some reformating

--- ms5611.fs
+++ ms5611.fs
@@ -1,7 +1,7 @@
 $77 constant MS5611.ADDR
 
-12 buffer: params \ calibration data
-6 buffer: values \ rawdata
+12 buffer: MS5611.params \ calibration data
+6 buffer: MS5611.values \ rawdata
 
 : MS5611-reset 
   MS5611.ADDR i2c-addr
@@ -21,7 +21,7 @@ $77 constant MS5611.ADDR
 
 : MS5611-calib
   
-  params
+  MS5611.params
   6 0 do
 		2 $a2 I 2 * + ms5611-rd
   loop
@@ -29,46 +29,46 @@ $77 constant MS5611.ADDR
 ;
 
 : ms5611-rawt
-MS5611.ADDR i2c-addr
-$58 >i2c 0 i2c-xfer drop
-10 ms
-values 3 0 ms5611-rd drop
-values dup c@ 16 lshift swap 1+ dup c@ 8 lshift swap 1+ c@ + +	
+  MS5611.ADDR i2c-addr
+  $58 >i2c 0 i2c-xfer drop
+  10 ms
+  MS5611.values 3 0 ms5611-rd drop
+  MS5611.values dup c@ 16 lshift swap 1+ dup c@ 8 lshift swap 1+ c@ + +	
 ;
 
 : ms5611-rawp
-MS5611.ADDR i2c-addr
-$48 >i2c 0 i2c-xfer drop
-10 ms
-values 3 + 3 0 ms5611-rd drop
-values 3 + dup c@ 16 lshift swap 1+ dup c@ 8 lshift swap 1+ c@ + +	
+  MS5611.ADDR i2c-addr
+  $48 >i2c 0 i2c-xfer drop
+  10 ms
+  MS5611.values 3 + 3 0 ms5611-rd drop
+  MS5611.values 3 + dup c@ 16 lshift swap 1+ dup c@ 8 lshift swap 1+ c@ + +
 ;
 
 
 : ms5611-temp
 
-ms5611-rawt
-params dup 8 + c@ 8 lshift swap 9 + c@ + 256 * -
-params dup 10 + c@ 8 lshift swap 11 + c@ + *
-8388608 / 2000 +
+  ms5611-rawt
+  MS5611.params dup 8 + c@ 8 lshift swap 9 + c@ + 256 * -
+  MS5611.params dup 10 + c@ 8 lshift swap 11 + c@ + *
+  8388608 / 2000 +
 ;
 
 : ms5611-press
-	ms5611-rawt
-	params dup 8 + c@ 8 lshift swap 9 + c@ + 256 * -
-	dup
+  ms5611-rawt
+  MS5611.params dup 8 + c@ 8 lshift swap 9 + c@ + 256 * -
+  dup
 		
-	params dup 6 + c@ 8 lshift swap 7 + c@ + * 128 / s>d
-	params dup 2 + c@ 8 lshift swap 3 + c@ + 65536 um* d+
-	dnegate
+  MS5611.params dup 6 + c@ 8 lshift swap 7 + c@ + * 128 / s>d
+  MS5611.params dup 2 + c@ 8 lshift swap 3 + c@ + 65536 um* d+
+  dnegate
+
+  rot
 	
-	rot
+  MS5611.params dup 4 + c@ 8 lshift swap 5 + c@ + * 256 / s>d
+  MS5611.params dup 0 + c@ 8 lshift swap 1 + c@ + 32768 um* d+
 	
-	params dup 4 + c@ 8 lshift swap 5 + c@ + * 256 / s>d
-	params dup 0 + c@ 8 lshift swap 1 + c@ + 32768 um* d+
-	
-	ms5611-rawp s>d ud* 2097152 s>d d/ d+
-	32768 um/mod swap drop
+  ms5611-rawp s>d ud* 2097152 s>d d/ d+
+  32768 um/mod swap drop
 ;
 
 : MS5611-height
@@ -81,7 +81,6 @@ ms5611-reset
 100 ms
 ms5611-calib
 
-6 buffer: hoehe
 
 : vario. 
   2200
